2dd122a1edec008656b3442364a06edde0248eab
talos <talos@chinawebeye.com>
2016-10-13 14:53:36 +0800

    change statusbar background

--- app/src/main/java/com/vita/weather/DetailsActivity.java
+++ app/src/main/java/com/vita/weather/DetailsActivity.java
@@ -82,6 +82,17 @@ public class DetailsActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus) {
+//            getWindow().getDecorView().setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN);}
+    }
+
+    @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(STATE_CURRENT_PAGE_POSITION, mCurrentPosition);
